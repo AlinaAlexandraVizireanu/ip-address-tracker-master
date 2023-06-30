f56fc2a22f188496ff4e02d87d70c1d30ff56f92
Alina Vizireanu <vizireanualina15@yahoo.com>
2023-06-30 17:23:38 +0200

    updated version of the solution

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const ipInfo = document.querySelector("#ip-info");
 const locationInfo = document.querySelector("#location-info");
 const timezoneInfo = document.querySelector("#timezone-info");
 const ispInfo = document.querySelector("#isp-info");
+let marker;
 
 // ----------Configuration of the map----------
 
@@ -20,47 +21,51 @@ L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
 
-L.marker([51.505, -0.09], { icon: locationIcon })
+marker = L.marker([51.505, -0.09], { icon: locationIcon })
   .addTo(map)
-  .bindPopup("A pretty CSS popup.<br> Easily customizable.")
+  .bindPopup("Default location")
   .openPopup();
 
 // ----------Configuration of the map----------
 
 form.addEventListener("submit", function (e) {
-    e.preventDefault();
-    let regexp = /^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/g;
-  
-    axios
-      .get(
-        `https://geo.ipify.org/api/v2/country,city?apiKey=at_mExeSqk8jHaRZjAEgponMhR3QlYDH&${
-          regexp.test(textInput.value)
-            ? "ipAddress=" + textInput.value
-            : "domain=" + textInput.value
-        }`
-      )
-      .then((response) => {
-        let ip = response.data.ip;
-        let location = `${response.data.location.city}, ${response.data.location.country} ${response.data.location.postalCode}`;
-        let timezone = `UTC${response.data.location.timezone}`;
-        let isp = response.data.isp;
-        let lat = response.data.location.lat;
-        let lng = response.data.location.lng;
-  
-        ipInfo.innerText = ip;
-        locationInfo.innerText = location;
-        timezoneInfo.innerText = timezone;
-        ispInfo.innerText = isp;
-  
-        L.marker([lat, lng], { icon: locationIcon })
-          .addTo(map)
-          // .bindPopup("A pretty CSS popup.<br> Easily customizable.")
-          // .openPopup();
-      })
-      .catch((err) => {
-        console.log("Something went wrong!");
-        console.log(err);
-      });
-  });
+  e.preventDefault();
+  let regexp = /^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/g;
 
+  axios
+    .get(
+      `https://geo.ipify.org/api/v2/country,city?apiKey=at_mExeSqk8jHaRZjAEgponMhR3QlYDH&${
+        regexp.test(textInput.value)
+          ? "ipAddress=" + textInput.value
+          : "domain=" + textInput.value
+      }`
+    )
+    .then((response) => {
+      let ip = response.data.ip;
+      let location = `${response.data.location.city}, ${response.data.location.country} ${response.data.location.postalCode}`;
+      let timezone = `UTC${response.data.location.timezone}`;
+      let isp = response.data.isp;
+      let lat = response.data.location.lat;
+      let lng = response.data.location.lng;
 
+      ipInfo.innerText = ip;
+      locationInfo.innerText = location;
+      timezoneInfo.innerText = timezone;
+      ispInfo.innerText = isp;
+
+      if (marker) {
+        map.removeLayer(marker);
+      }
+
+      marker = L.marker([lat, lng], { icon: locationIcon })
+        .addTo(map)
+        .bindPopup("Location of the requested IP Address or domain")
+        .openPopup();
+
+        map.setView([lat, lng], 13);
+    })
+    .catch((err) => {
+      alert("Please introduce a valid IP address or domain");
+      console.log(err);
+    });
+});
