ae510919680e2aa43ad9b35f17d7e0f4e064a684
Alina Vizireanu <vizireanualina15@yahoo.com>
2023-07-03 12:43:52 +0200

    update bugfixes

--- app.js
+++ app.js
@@ -31,45 +31,45 @@ marker = L.marker([51.505, -0.09], { icon: locationIcon })
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
-  if(textInput.innerText === ""){
-    alert("Please introduce an IP address or a domain")
+  if (textInput.value === "") {
+    alert("Please introduce an IP address or a domain");
+  } else {
+    let regexp = /^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/g;
+
+    axios
+      .get(
+        `https://geo.ipify.org/api/v2/country,city?apiKey=at_mExeSqk8jHaRZjAEgponMhR3QlYDH&${
+          regexp.test(textInput.value)
+            ? "ipAddress=" + textInput.value
+            : "domain=" + textInput.value
+        }`
+      )
+      .then((response) => {
+        let ip = response.data.ip;
+        let location = `${response.data.location.city}, ${response.data.location.country} ${response.data.location.postalCode}`;
+        let timezone = `UTC${response.data.location.timezone}`;
+        let isp = response.data.isp;
+        let lat = response.data.location.lat;
+        let lng = response.data.location.lng;
+
+        ipInfo.innerText = ip;
+        locationInfo.innerText = location;
+        timezoneInfo.innerText = timezone;
+        ispInfo.innerText = isp;
+
+        if (marker) {
+          map.removeLayer(marker);
+        }
+
+        marker = L.marker([lat, lng], { icon: locationIcon })
+          .addTo(map)
+          .bindPopup("Location of the requested IP Address or domain")
+          .openPopup();
+
+        map.setView([lat, lng], 13);
+      })
+      .catch((err) => {
+        alert("Please introduce a valid IP address or domain");
+      });
   }
-  let regexp = /^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/g;
-
-  axios
-    .get(
-      `https://geo.ipify.org/api/v2/country,city?apiKey=at_mExeSqk8jHaRZjAEgponMhR3QlYDH&${
-        regexp.test(textInput.value)
-          ? "ipAddress=" + textInput.value
-          : "domain=" + textInput.value
-      }`
-    )
-    .then((response) => {
-      let ip = response.data.ip;
-      let location = `${response.data.location.city}, ${response.data.location.country} ${response.data.location.postalCode}`;
-      let timezone = `UTC${response.data.location.timezone}`;
-      let isp = response.data.isp;
-      let lat = response.data.location.lat;
-      let lng = response.data.location.lng;
-
-      ipInfo.innerText = ip;
-      locationInfo.innerText = location;
-      timezoneInfo.innerText = timezone;
-      ispInfo.innerText = isp;
-
-      if (marker) {
-        map.removeLayer(marker);
-      }
-
-      marker = L.marker([lat, lng], { icon: locationIcon })
-        .addTo(map)
-        .bindPopup("Location of the requested IP Address or domain")
-        .openPopup();
-
-      map.setView([lat, lng], 13);
-    })
-    .catch((err) => {
-      alert("Please introduce a valid IP address or domain");
-      console.log(err);
-    });
 });
